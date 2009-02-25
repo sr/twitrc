051aee391dc30825ccc3677967ed0e0cc52ec6d9
Syd <syd@scrimfind.org>
2009-02-25 13:12:21 +1000

    rewrote, afew more things work.

---
+++ lib/irc_numconst.rb
@@ -0,0 +1,44 @@
+RPL_NONE = 300
+RPL_USERHOST = 302
+RPL_ISON = 303
+RPL_AWAY = 301
+RPL_UNAWAY = 305
+RPL_NOWAWAY = 306
+RPL_WHOISUSER = 311
+RPL_WHOISSERVER = 312
+RPL_WHOISOPERATOR = 313
+RPL_WHOISIDLE = 317
+RPL_ENDOFWHOIS = 318
+RPL_WHOISCHANNELS = 319
+RPL_WHOWASUSER = 314
+RPL_ENDOFWHOWAS = 369
+RPL_LISTSTART = 312
+RPL_LIST = 322
+RPL_LISTEND = 323
+RPL_CHANNELMODEIS = 324
+RPL_NOTOPIC = 331
+RPL_TOPIC = 332
+RPL_INVITING = 341
+RPL_SUMMONING = 342
+#incomplete
+
+RPL_MOTDSTART=375
+                #":- <server> Message of the day - "
+RPL_MOTD=372
+                #":- <text>"
+RPL_ENDOFMOTD=376
+                #":End of /MOTD command"
+                
+RPL_NAMREPLY=353
+                #"<channel> :[[@|+]<nick> [[@|+]<nick> [...]]]"
+RPL_ENDOFNAMES=366
+                #"<channel> :End of /NAMES list"
+
+
+
+
+
+
+
+
+
--- twirc.rb
+++ twirc.rb
@@ -1,97 +1,99 @@
 require 'rubygems'
 require 'eventmachine'
 require 'twitter'
-require 'cgi'
 require 'activesupport'
+require 'lib/irc_numconst'
 
-module Ircd
-  @username = ""
-  @pass = ""
-  @nickname = ""
-  
+$debug = 1
+$servername = "twitter.irc"
+module TwitRC
+@username = ""
+@password = ""
+@nickname = ""
+
   def send_data(data)
-    super  
-    puts ">> #{data}"
+    super
+    puts "<< %s" % data if $debug
   end
   
   def receive_data(data)
-    if data.match(/\n{1}/)
-      data.split(/\r?\n/).each do |x| process_data(x) end
-    else
-      process_data(data.strip)
-    end
+    data.match(/\n{1}/) ? data.split(/\r?\n/).each{|x|process_data(x)}:process_data(data.strip)
   end
   
   def process_data(data)
-    data.strip
-    puts "<< #{data}"
-    case data
-      when /^PRIVMSG (.*?) :(.*)$/
-        @twit.update($2)
-      when /^USER (.*?) /m
-        @username = $1
-        puts "#{@username} - #{@pass} - #{@nickname}"
-        @twit = Twitter::Base.new(@username, @pass)
-        connect_user()
-      when /^PASS (.*)/
-        @pass = $1
-      when /^NICK (.*)/
-        @nickname = $1
-      when /^WHO \#twitter/
-        @twit.friends.each do |u|
-          send_data(":localhost 352 #{u.screen_name} #twitter #{u.screen_name} twitter localhost #{u.screen_name} H :0 #{u.name}")
-        end
-        servermsg("End of /WHO list.", 315, "#twitter")
-      when /^NAMES \#twitter/
-        names = String.new
-        @twit.friends.each do |u| 
-          names << "#{u.screen_name} "
-        end
-        servermsg("@twitter #{names}", 353, "#twitter")
-        servermsg("End of /NAMES list.", 366, "#twitter")
+    puts ">> %s" % data if $debug
+    # we assume that if there is no leading colon here, we're dealing with a command and call a method
+    if data =~ /^:{0}/ then
+      self.send("irc_%s"%data.match(/^(.*?) (.*)$/)[1].downcase,$2)
     end
   end
   
-  def connect_user()
-    servermsg("*** Connecting to twitter", "NOTICE AUTH")
-    send_data ":#{@nickname}!localhost JOIN :#twitter\n"
-    send_data ":localhost MODE #twitter +ns\n"
-    names = String.new
-    @twit.friends.each do |u| 
-      names << "#{u.screen_name} "
-    end
-    servermsg("@twitter #{names}", 353, "#twitter")
-    servermsg("End of /NAMES list.", 366, "#twitter")
-    @twit.friends.each do |u|
-      privmsg(u.screen_name,CGI::unescapeHTML(u.status.text),"#twitter")
-    end
+  def irc_pass(password)
+    @password = password
+    puts "recieved password #{password}"
+  end
+  
+  def irc_nick(nickname)
+    @nickname = nickname
+    puts "recieved nick #{nickname}"
+  end
+  
+  def irc_user(username)
+    @username = username.match(/^(.*?) /)[1]
+    do_twitter_connection()
+  end
+  
+  def irc_privmsg(message)
+    @twit.update(message.gsub(/^.*?:/,""))
+  end
+
+  def irc_quit(message)
+    close_connection
+  end
+  
+  def method_missing(id, *args)
+    puts "*** this feature is currently unsupported (#{id})"
+    pp args 
   end
   
   def privmsg(sender, message, channel=nil)
     send_data ":#{sender} PRIVMSG #{channel} :#{message}\n"
   end
   
-  def servermsg(message, id=nil, channel=nil)
-    send_data ":localhost #{id} #{@nickname} #{channel} :#{message}\n"
+  def do_twitter_connection()
+    @twit = Twitter::Base.new(@username, @password)
+     rpl RPL_MOTDSTART, ":#{$servername} message of the day - "
+     rpl RPL_MOTD, ":thank you for using this server"
+     rpl RPL_ENDOFMOTD, ":End of /MOTD command"
+     send_data ":#{@nickname}!#{$servername} JOIN :#twitter\n"
+     send_data ":#{$sername} MODE #twitter +ns\n"
+     names = ""
+     @twit.friends.each do |u| 
+       names << "#{u.screen_name} "
+     end
+     rpl RPL_NAMREPLY, ":@twitter #{names}", "#twitter"
+     rpl RPL_ENDOFNAMES, ":End of /NAMES list.", "#twitter"
+      @twit.friends.each do |u|
+        privmsg(u.screen_name,CGI::unescapeHTML(u.status.text),"#twitter")
+      end
   end
-  
-  def post_init()
-    puts "== new connection =="
+
+  def rpl(num, message, channel=nil)
+    #send_data ":localhost #{id} #{@nickname} #{channel} :#{message}\n"
+    send_data ":#{$servername} #{num} #{@nickname} #{channel} #{message}\n"
   end
   
   def initialize()
     time = Time.now
     EventMachine::add_periodic_timer(5.minutes) do
-    @twit.timeline(:friends, :since => Time.now.in_time_zone("Eastern Time (US & Canada)") - 5.minutes).each do |u|
-      privmsg(u.user.screen_name,CGI::unescapeHTML(u.text), "#twitter")
-    end
-    
-    send_data "PING :localhost\n"
+      @twit.timeline(:friends, :since => Time.now.in_time_zone("Eastern Time (US & Canada)") - 5.minutes).each do |u|
+        privmsg(u.user.screen_name,CGI::unescapeHTML(u.text), "#twitter")
+      end
     end
   end
-
+  
 end
 
-EM.run {
-  EM.start_server "0.0.0.0", 10000, Ircd
+EventMachine::run {
+  EventMachine::start_server "127.0.0.1", 10000, TwitRC
 }
